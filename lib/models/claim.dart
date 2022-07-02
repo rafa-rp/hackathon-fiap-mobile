432b5e1ccdc3d7cc33584cad013f617251443d00
Rafael <rafa.rp22@gmail.com>
2022-07-02 19:35:47 -0300

    Esqueleto da chamada ao endpoint

---
+++ lib/models/claim.dart
@@ -0,0 +1,24 @@
+class Claim {
+  Claim({
+    required this.title,
+    required this.author,
+    required this.type,
+    required this.channel,
+    required this.situation,
+    required this.image,
+    required this.description
+
+
+
+
+  });
+
+  String title;
+  String author;
+  String type;
+  String channel;
+  String situation;
+  String image;
+  String description;
+
+}
--- lib/screens/claims_screen.dart
+++ lib/screens/claims_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:hackathon_fiap_mobile/models/claim.dart';
+import 'package:http/http.dart';
 
 class ClaimsScreen extends StatefulWidget {
   static const String id = '/claims';
@@ -10,7 +14,7 @@ class ClaimsScreen extends StatefulWidget {
 }
 
 class _ClaimsScreenState extends State<ClaimsScreen>{
-  List<Claims> claimsList = [];
+  List<Claim> claimsList = [];
 
   @override
   void initState(){
@@ -22,10 +26,82 @@ class _ClaimsScreenState extends State<ClaimsScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todas Reclamações'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Todas Reclamações'),
+        ),
+        body: ListView.separated(
+          itemCount: claimsList.length,
+          itemBuilder: (context, index) {
+            final claim = claimsList[index];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 150,
+                child: Material(
+                  elevation: 8,
+                  borderRadius: BorderRadius.circular(16),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                        ),
+                        child: Image.network(claim.image),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              claim.title,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              claim.description,
+                              maxLines: 5,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => const Divider(),
+        ));
   }
 
+  void fetchClaims() async {
+    final uri = Uri.parse('https://demo7206081.mockable.io/movies');
+    Response response = await Client().get(uri);
+
+    final responseJson = jsonDecode(response.body);
+
+    claimsList = responseJson['results']
+        .map<Claim>(
+          (json) => Claim(
+        title: json['title'],
+        author: json['author'],
+        description: json['description'],
+        image: json['image'],
+        situation: json['situation'],
+        channel: json['channel'],
+        type: json['type']
+      ),
+    )
+        .toList();
+
+    setState(() => {});
+  }
 }
